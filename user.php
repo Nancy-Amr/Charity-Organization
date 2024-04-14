7e4041fabda717eb07e7e1d49de4764632288de1
ranas27 <133527989+ranas27@users.noreply.github.com>
2024-04-14 23:00:19 +0200

    Update user.php

--- user.php
+++ user.php
@@ -24,6 +24,7 @@
                     <td>Phone</td>
                     <td>Adrees</td>
                     <td>Email</td>
+                    <td>Password</td>
                     <td>Edit</td>
                     <td>Delete</td>
             </tr> 
