3114fa4b867a630672a66090fd95dd7f21890d23
Menna Ayman <154658784+mennaaymaneng@users.noreply.github.com>
2024-04-14 21:04:52 +0200

    Create AddOppForm.php

---
+++ AddOppForm.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Oppurtunity Info Insertion</title>
+    <meta name="description" content="Oppurtunity insertion">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css"/>
+    
+
+
+</head>
+<body>
+<h1>Please insert Oppurtunity info:</h1>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        <table>
+            <tr>
+                <td>Id:</td>
+                <td><input type="text" name="Id"></td>
+            </tr>
+            <tr>
+                <td>title:</td>
+                <td><input type="text" name="title"></td>
+            </tr>
+            <tr>
+                <td>volunteer:</td>
+                <td><input type="text" name="volunteer"></td>
+            </tr>
+            <tr>
+                <td>location:</td>
+                <td><input type="text" name="location"></td>
+            </tr>
+            <tr>
+                <td>Date:</td>
+                <td><input type="text" name="date"></td>
+            </tr>
+           
+            </tr>
+            <tr>
+                <td colspan="2"><input type="submit"></td>
+            </tr>
+        </table>
+    </form>
+   
+    <?php
+include_once"Function.php";
+$obj = new VolunteeringOppurtunity();
+$obj->InsertOpp();
+       
+?>
+</table>
+</body>
+</html>
