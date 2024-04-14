1b1cf5042e6686ba1f92c7f3e7560d791f92ed9d
Nancy-Amr <144449880+Nancy-Amr@users.noreply.github.com>
2024-04-14 19:17:04 +0200

    Update userTypeForm.php

--- userTypeForm.php
+++ userTypeForm.php
@@ -1,53 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Edit User Types</title>
+    <title>User Type Info Insertion</title>
+    <meta name="description" content="my first trial">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css"/>
+
 </head>
 <body>
-    <h2>Edit User Types</h2>
+<h1>Please insert your info:</h1>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        <tr>
+            Type:<input type="text" name="type"></td><br>
+            ID:<input type="text" name="id"></td><br>
+           
+        </tr>
+        <tr>
+            <td colspan="2"><input type="submit" ></td>
+        </tr>
+    </form>
+   
     <?php
-    include_once "Function.php";
-    $obj=new UserType();
-    // Check if form is submitted
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Call function to handle user edit
-       $obj-> handleTypeEdit();
-        
-        // Redirect to user page after editing
-        header("Location: userT.php");
-        exit();
-    } else {
-        // Retrieve user data if ID is provided
-        if (isset($_GET['id']) && $_GET['id'] !== '') {
-            $userId = $_GET['id'];
-            $filename = $obj->UTmainobj->filename;
-            $file = fopen($filename, "r") or die("Unable to open file!");
-
-            // Iterate through each line in the file
-            while (!feof($file)) {
-                $line = fgets($file);
-                $usertype = explode($obj->UTmainobj->separator, $line);
-
-                if (!empty($usertype) && $usertype[0] == $userId) {
-                    list($id, $type) = $usertype;
-    ?>
-                    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-                        <input type="hidden" name="id" value="<?php echo $id; ?>">
-                        
-                        Type: <input type="text" name="Type" value="<?php echo $type; ?>"><br>
-
-                        <input type="submit" name="edit" value="Save Changes">
-                    </form>
-    <?php
-                    break;
-                }
-            }
-            fclose($file);
-        } else {
-            echo "User ID not provided.";
-        }
-    }
-    ?>
+include_once"Function.php";
+$obj = new UserType();
+$obj->InsertType();
+?>
+</table>
 </body>
 </html>
