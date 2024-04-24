6eea6daa3055b66fa5bea5b6cbb3d158cd8eafb6
21P0124 21P0124 <21P0124@eng.asu.edu.eg>
2024-04-24 17:25:07 +0200

    delete user type doesnt delete user

--- DeleteTypeForm.php
+++ DeleteTypeForm.php
@@ -2,14 +2,12 @@
 
 include_once "Function.php";
 $obj=new UserType();
-$user= new User();
 if (isset($_GET['id']) && $_GET['id'] !== '') {
     // The delete button is clicked
     // Perform deletion logic here
     $IdToDelete = $_GET['id'];
     // Call a function to handle user deletion
     $obj->deleteType($IdToDelete, $obj->UTmainobj->filename);
-    $user->deleteUser($IdToDelete, $user->mainobj->filename);
     // Redirect back to the user.php page after deletion
     header("Location: userT.php");
     exit(); // Ensure no further code execution after redirection
