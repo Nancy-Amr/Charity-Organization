71e8657fb446012f90a53bd7c71d169b462a0ae9
Nancy-Amr <144449880+Nancy-Amr@users.noreply.github.com>
2024-05-10 11:02:39 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,12 @@
+
 <?php
+
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
-    $id = $_POST["id"];
+    //$id = $_POST["id"];
+    $email = $_POST["email"];
     $password = $_POST["password"];
     $filename = "user.txt";
     $file = file($filename);
@@ -11,17 +15,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     foreach ($file as $line) {
         $userData = explode("~", $line);
+
+        if (isset($userData) && !empty($userData)) {
+            // Access userData elements here
+            if ($userData[4] == $email) {
         
-        if ($userData[0] == $id) {
+                if ($userData[4] == $email) {
             // Check if the password matches
-            if (trim($userData[5]) == $password) {
+                    if (trim($userData[5]) == $password) {
                 
-                $flag = true;
+                    $flag = true;
             }
             break; 
         }
     }
-    
+}
+    }
     if($flag){
         if ($userData[4] == "admin@email.com") {
             header("Location: admin.php?Username=" . urlencode($userData[1]));
@@ -43,3 +52,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
 }
 ?>
+
