686f66aa0a097cbc3703597b54d0a66624e5cf4e
Nancy-Amr <144449880+Nancy-Amr@users.noreply.github.com>
2024-05-10 11:08:49 +0300

    Update Donation.php

--- View/Donation.php
+++ View/Donation.php
@@ -5,13 +5,32 @@
     <meta  name ="description" content = "">
     <meta  name ="viewport" content ="width= device-width, initial scale=1">
     <script src="" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" media="screen" href='main.css'/>
+    <link rel="stylesheet" type="text/css" media="screen" href='../main.css'/>
     <script src='main.js'></script>
     <style>
         table, th, td {
          border: 1px solid black;
          padding: 5px;
+         margin-bottom: 40px;
   }
+  button {
+                /* Adjust these properties as desired */
+                padding: 10px 20px; /* Adjust padding for button size */
+                border: 1px solid #45A049; /* Border color */
+                border-radius: 5px; /* Rounded corners */
+                font-size: 16px; /* Font size for button text */
+                cursor: pointer; /* Change cursor to pointer on hover */
+                margin-left: 20px;
+              }
+              
+              /* Optional: Hover effect for all buttons */
+              button:hover {
+                background-color: #45A049; /* Background color on hover */
+              }
+              .button-container {
+                display: flex; /* Enable flexbox for the container */
+                justify-content: center; /* Center buttons horizontally */
+              }
   </style>
     </style>
   </head>
@@ -43,11 +62,13 @@ for($i=0;$i<count($arr);$i++){
 ?>
 
 </table>
+<div class="button-container">
 <button onclick="location.href='../Controllers/DonationDetailsController.php?Command=Add';">Insert New Donation</button><br>
 <button onclick="location.href='DonationDetails.php';">View All Donations details</button><br>
 <button onclick="location.href='DonationType.php';">View Donation Types</button><br>
 <button onclick="location.href='user.php';">View Users</button>
 <br><button onclick="location.href='VolunteeringOppurtunity.php';">View Volunteering Oppurtunities</button>
+</div>
 
 </body>
 </html>
