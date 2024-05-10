63c6f8fb45ed30776f339ff69cdb8f6b1af9d56b
Nancy-Amr <144449880+Nancy-Amr@users.noreply.github.com>
2024-05-10 11:03:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+   
+   
     
 </head>
 <body>
-    <h2>Login</h2>
+<h2 style="text-align: center;">Login</h2>
     <form action="login.php" method="post">
-        <label >ID:</label>
-        <input type="text" id="id" name="id" required><br><br>
+        <!-- <label >ID:</label>
+        <input type="text" id="id" name="id" required><br><br> -->
+        <label >Email:</label>
+        <input type="text" id="email" name="email" required><br><br>
         <label >Password:</label>
         <input type="password" id="password" name="password" required><br><br>
         <input type="submit" value="Login">
     </form>
 </body>
 </html>
+
