f3abe1717a6b9d22e8b276572f331c063b5d32ef
Nancy-Amr <144449880+Nancy-Amr@users.noreply.github.com>
2024-05-10 11:06:21 +0300

    Update AddDonationTypeForm.php

--- View/AddDonationTypeForm.php
+++ View/AddDonationTypeForm.php
@@ -8,7 +8,7 @@ class GenerateDonationTypeForm {
     <title>Donation Type Insertion</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css"/>
+    <link rel="stylesheet" type="text/css" href="../style.css">
 
 </head>
 <body>
