6e97b7a25fec52d1afcc096356bdc685f9b70cd4
MennaEzzelarab <menna.ezzelarab@gmail.com>
2024-05-15 15:58:10 +0300

    Returned old DonationDetailsController

--- Controllers/DonationDetailsController.php
+++ Controllers/DonationDetailsController.php
@@ -1,28 +1,25 @@
+
 <?php
-include_once "../Models/DonationDetails/DonationDetailsClass.php";
-include_once "../Models/DonationType/DonationTypeClass.php";
-include_once "../Controllers/DonationController.php";
-include_once "../View/DonationDetailsView.php";
-include_once "../View/AddDonationForm.php";
+include_once"../Models/DonationDetails/DonationDetailsClass.php";
+include_once"../Models/DonationType/DonationTypeClass.php";
+include_once"../Controllers/DonationController.php";
+include_once"../View/DonationDetailsView.php";
+include_once"../View/AddDonationForm.php";
+$Command=$_GET["Command"];
 
-$Command = $_GET["Command"];
-
-if ($Command == "Show") {
-    // Example of showing donation details
-    $obj = new DonationDetails();
-    $type = new DonationType();
-    $objView = new DonationDetailsView();
-    $don = $obj->getById($_GET["Id"]);
-    $t = $type->getById($don->TypeId);
-    $objView->showDonationDetails($don, $t);
+if ($Command=="Show"){
+$obj=new DonationDetails();
+$type=new DonationType();
+$objview= new DonationDetailsView();
+$don=$obj->getById($_GET["Id"]);
+$t=$type->getById($don->TypeId);
+$objview->showDonationDetails($don,$t);
 }
-
-if ($Command == "Add") {
-    // Example of adding a new donation
+if ($Command=="Add"){
+   
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $obj = new DonationDetails();
 
-        // Get data from the form submission
         $date = $_POST["Date"];
         $donorId = $_POST["DonorId"];
         $RecipientId = $_POST["RecipientId"];
@@ -30,27 +27,21 @@ if ($Command == "Add") {
         $feedback = $_POST["Feedback"];
         $TypeId = $_POST["DonationTypeId"];
         $Rating = $_POST["Rating"];
-
-        // Generate a unique ID
-        $lastId = $obj->mainobj->getLastId($obj->mainobj->filename, $obj->mainobj->separator);
+        $lastId = $obj->mainobj->getLastId($obj->mainobj->filename,$obj->mainobj->separator);
         $id = $lastId + 1;
-
-        // Prepare donation information
         $DonationInfo = "$id~$date~$RecipientId~$donorId~$feedback~$time~$Rating~$TypeId\n";
-
-        // Insert the donation
         $obj->Insert($DonationInfo);
-    } else {
-        // Show the form to add a new donation
-        $newObj = new GenerateDonationForm();
-        $newObj->generateDonationForm();
-    }
-}
+        
 
-if ($Command == "Edit") {
-    // Example of editing an existing donation
+    }
+    else{$newobj= new GenerateDonationForm();
+        $newobj->generateDonationForm();}
+    
+       
+}
+if($Command=="Edit"){
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get data from the form submission
+
         $id = $_POST["id"];
         $date = $_POST["Date"];
         $donorId = $_POST["DonorId"];
@@ -59,24 +50,27 @@ if ($Command == "Edit") {
         $feedback = $_POST["Feedback"];
         $TypeId = $_POST["DonationTypeId"];
         $Rating = $_POST["Rating"];
-
-        // Prepare donation information
         $DonationInfo = "$id~$date~$RecipientId~$donorId~$feedback~$time~$Rating~$TypeId\n";
 
-        // Handle the donation edit
         $obj = new DonationDetails();
         $obj->handleEdit($DonationInfo);
+
+       
     }
+   
 }
 
-if ($Command == "Delete") {
-    // Example of deleting a donation
-    $obj = new DonationDetails();
+if($Command=="Delete"){
+    $obj=new DonationDetails();
     if (isset($_GET['id']) && $_GET['id'] !== '') {
         $DonationIdToDelete = $_GET['id'];
         $obj->delete($DonationIdToDelete);
         header("Location:../View/DonationDetails.php");
-        exit();
+        exit(); 
     }
+
 }
+   
+
+?>
 
